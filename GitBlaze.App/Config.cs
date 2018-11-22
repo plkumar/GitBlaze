205dc02a7e2be533779b10f85562d6348d2c15f7
Lakshman Peethani <peethani@outlook.com>
2018-11-22 09:16:47 +0530

    added config

---
+++ GitBlaze.App/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GitBlaze.App
+{
+    public class Config
+    {
+        private static string s_gitKey = string.Empty;
+
+        public Config()
+        {
+        }
+
+        public static string GitKey { get => s_gitKey; set => s_gitKey = value; }
+    }
+}
